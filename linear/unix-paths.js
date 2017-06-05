1f909373b6ea50e951dd3e70d60ef11a9ebcf0f6
Charlie Harrington <charlie.harrington@lendup.com>
2017-06-05 10:17:04 -0700

    unix path with stack

--- linear/unix-paths.js
+++ linear/unix-paths.js
@@ -1,6 +1,43 @@
+"use strict";
 /*
 
-given a UNIX path "/foo/bar/../bar/./../bar"
+given a UNIX path "/foo/bar/../bar/./../bar/"
 reduce to simplest: "/foo/bar/"
 
 */
+var assert = require("assert");
+var Stack = require("./stack");
+
+var pathy = function(path) {
+  var stack = new Stack();
+  var word = "";
+  for (var i = 0; i < path.length; i++) {
+    var char = path.charAt(i);
+    if (char === "/") {
+      if (word.length) {
+        if (word === "..") {
+          stack.pop();
+        } else if (word === ".") {
+          // no op
+        } else {
+          stack.push(word);
+        }
+        word = "";
+      }
+    } else {
+      word += char;
+    }
+  }
+
+  var result = "/";
+  while (!stack.is_empty()) {
+    var next = stack.pop();
+    result = next + result;
+    result = "/" + result;
+  }
+
+  return result;
+
+};
+
+console.log(pathy("/foo/bar/../bar/./../bar/"));
